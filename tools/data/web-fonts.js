48a206391190e41d6c5eb31e68fbf2f9e3f5ca9c
webdevbynight <git@victor-brito.dev>
2025-02-28 23:18:20 +0100

    Initial commit

---
+++ tools/data/web-fonts.js
@@ -0,0 +1,13 @@
+/**
+ *  The fonts and weights to install must be declared as follows:
+ * {
+ *   "Name_of_the_font": ["Bold", "Italic", "Regular"],
+ *   "Name_of_the_variable_font": ["Italic-VariableFont_wght", "VariableFont_wght"]
+ * }
+ */
+const webFontsToInstall = {
+  Alata: ["Regular"],
+  Josefin_Sans: ["VariableFont_wght"]
+};
+
+export default webFontsToInstall;
